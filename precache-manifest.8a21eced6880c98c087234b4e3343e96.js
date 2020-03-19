85e7efd8ffd5f1cc665d97e2e79dcd0833548efb
oeighteyes <40931884+bryanlyq@users.noreply.github.com>
2020-03-19 18:15:28 +0800

    Updates

---
+++ precache-manifest.8a21eced6880c98c087234b4e3343e96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8be1fbeca6a2685e09d9b2aa70229a",
+    "url": "/starwars2/index.html"
+  },
+  {
+    "revision": "9d28f9b21c4774861f5f",
+    "url": "/starwars2/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5e699a09b4f5e914979b",
+    "url": "/starwars2/static/css/main.1c5ab9bb.chunk.css"
+  },
+  {
+    "revision": "9d28f9b21c4774861f5f",
+    "url": "/starwars2/static/js/2.10ac24cc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/starwars2/static/js/2.10ac24cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e699a09b4f5e914979b",
+    "url": "/starwars2/static/js/main.76435a6c.chunk.js"
+  },
+  {
+    "revision": "18a1a3eb6a9df7eb7537",
+    "url": "/starwars2/static/js/runtime-main.b4acd2a4.js"
+  },
+  {
+    "revision": "4a0c2e42bc61d247e7bcacc448909ee3",
+    "url": "/starwars2/static/media/starwarsbg.4a0c2e42.jpg"
+  }
+]);
